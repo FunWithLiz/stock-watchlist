dc3d21f12a7aa8a0ff3086158b0bfc33eb324862
Elizabeth Fun <elizabethfun@gmail.com>
2018-09-06 13:27:39 -0400

    creating new branch

--- rails-stock/config/routes.rb
+++ rails-stock/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :stocks, only: [:index]
   resources :personal_watchlists do 
-    resources :category_list, only: [:create, :update, :destroy]
+    resources :category_list, only: [:index, :create, :update, :destroy]
   end
 end
