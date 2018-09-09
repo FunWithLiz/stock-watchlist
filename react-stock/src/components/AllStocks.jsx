4873fab09f02d5fbcc63191fd7a06d5df51702f6
Elizabeth Fun <elizabethfun@gmail.com>
2018-09-09 14:20:42 -0400

    added hover over text for image

--- react-stock/src/components/AllStocks.jsx
+++ react-stock/src/components/AllStocks.jsx
@@ -4,26 +4,33 @@ function AllStocks(props) {
     const obj = props.stocks.stock
     return (
         <div>
-        <h2 className="stockTitle">List of all Stocks</h2>
-        <div className="allStocks">
-            {obj.map((stock) => (
-                <div key={stock.id}>
-                    <h4>
-                        {stock.id}{stock.database_code}
-                    </h4>
-                    <p>
-                        {stock.dataset_code}
-                        <br />
-                        {stock.frequency}
-                        <br />
-                        {stock.newest_available_date}
-                        <br />
-                        {stock.oldest_available_date}
-                    </p>
-                </div>
-            ))
-        }
-        </div>
+            <h2 className="stockTitle">List of all Stocks</h2>
+            <div className="allStocks">
+                {obj.map((stock) => (
+                    <div className="hover_img" 
+                    key={stock.id}>
+                        <h4>
+                            <b>
+                            {stock.id}
+                            <span className="stockimg"><img  src={require("../img/stock_graph.gif")} alt="bgimg"/></span>
+                            </b>
+                        </h4>
+                        <h4 className="stockspace">
+                            {stock.database_code}
+                        </h4>
+                        <p>
+                            {stock.dataset_code}
+                            <br />
+                            {stock.frequency}
+                            <br />
+                            {stock.newest_available_date}
+                            <br />
+                            {stock.oldest_available_date}
+                        </p>
+                    </div>
+                ))
+                }
+            </div>
         </div>
 
     )
